6d9610025e4bd14e05731b353c5df2e15027b016
moplatt <moritz.plattner03@gmail.com>
2025-05-14 11:38:05 +0200

    main.js update

--- main.js
+++ main.js
@@ -12,7 +12,9 @@ let map = L.map("map").setView([ibk.lat, ibk.lng], ibk.zoom);
 
 // thematische Layer
 let overlays = {
-    stations: L.featureGroup().addTo(map),
+    stations: L.featureGroup(),
+    temperature: L.featureGroup(),
+    wind: L.featureGroup().addTo(map),
 }
 
 // Layer control
@@ -26,6 +28,8 @@ L.control.layers({
     "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
 }, {
     "Wetterstationen": overlays.stations,
+    "Temperatur": overlays.temperature,
+    "Windgeschwindigkeit": overlays.wind,
 }).addTo(map);
 
 // Maßstab
@@ -38,6 +42,7 @@ async function loadStations(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
 
+    // Wetterstationen mit Icons und Popups
     L.geoJSON(jsondata, {
         attribution: "Datenquelle: <a href='https://lawinen.Report'>Lawine.Report</a>",
         pointToLayer: function (feature, latlng) {
@@ -49,15 +54,68 @@ async function loadStations(url) {
                 })
             });
         },
-        onEachFeature: function (feature, layer) {
-            console.log(feature);
+        onEachFeature: function(feature, layer) {
+            let pointInTime = new Date(feature.properties.date);
             layer.bindPopup(`
                 <h4>${feature.properties.name} (${feature.geometry.coordinates[2]}m)</h4>
-                `);
+                <ul>
+                  <li>Lufttemperatur (C) ${feature.properties.LT !== undefined ? feature.properties.LT : "-"}</li>
+                  <li>Relative Luftfeuchte (%) ${feature.properties.RH  || "-"}</li>
+                  <li>Windgeschwindigkeit (km/h) ${feature.properties.WG || "-"}</li>
+                  <li>Schneehöhe (cm) ${feature.properties.HS || "-"}</li>
+                </ul>
+                <span>${pointInTime.toLocaleString()}</span>
+            `);
         }
     }).addTo(overlays.stations)
-
-    // Wetterstationen mit Icons und Popups
-
+    showTemperature(jsondata);
+    showWind(jsondata);
 }
 loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
+
+function showTemperature(jsondata) {
+    L.geoJSON(jsondata, {
+        filter: function(feature) {
+            if (feature.properties.LT > -50 && feature.properties.LT < 50) {
+                return true;
+            }
+        },
+        pointToLayer: function(feature, latlng) {
+            let color = getColor(feature.properties.LT, COLORS.temperature);
+            return L.marker(latlng, {
+                icon: L.divIcon({
+                    className: "aws-div-icon",
+                    html: `<span style="background-color:${color}">${feature.properties.LT.toFixed(1)}</span>`
+                }),
+            })
+        },
+    }).addTo(overlays.temperature);
+}
+
+function showWind(jsondata) {
+    // TODO: add divIcons for windspeed feature.properties.WG
+    L.geoJSON(jsondata, {
+        filter: function(feature) {
+            if (feature.properties.WG >=0 && feature.properties.WG < 1000) {
+                return true;
+            }
+        },
+        pointToLayer: function(feature, latlng) {
+            let color = getColor(feature.properties.WG, COLORS.wind);
+            return L.marker(latlng, {
+                icon: L.divIcon({
+                    className: "aws-div-icon-wind",
+                    html: `<span style="background-color:${color}">${feature.properties.WG.toFixed(1)}</span>`,
+                })
+            })
+        },
+    }).addTo(overlays.wind);
+}
+
+function getColor(value, ramp) {
+    for (let rule of ramp) {
+        if (value >= rule.min && value < rule.max) {
+            return rule.color;
+        }
+    }
+}
