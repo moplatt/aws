43908b469f41e1fb9d1cc63c3545d13ede86292f
moplatt <moritz.plattner03@gmail.com>
2025-05-15 16:04:15 +0200

    style auf layer angewandt und background color auf color ändern

--- main.js
+++ main.js
@@ -164,8 +164,8 @@ function showDirection(jsondata) {
             return L.marker(latlng, {
                 icon: L.divIcon({
                     className: "aws-div-icon-wind",
-                    html: `<span style="transform:rotate(${feature.properties.WR}deg);
-                    background-color:${color}"><i class="fa-solid fa-circle-arrow-down">`
+                    html: `<span ><i style="transform:rotate(${feature.properties.WR}deg);
+                    color:${color}"class="fa-solid fa-circle-arrow-down">`
                 }),
             })
         },
